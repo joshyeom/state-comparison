ccda600f4906565bae180638d93e568d7d7a5ebc
joshyeom <jhyeom95@daum.net>
2024-06-07 18:59:00 +0900

    feat: contextAPI 구현

---
+++ src/components/ContextAPI.tsx
@@ -0,0 +1,29 @@
+import { ThemeStateType } from "../types/ThemeStateType"
+import { toggleTheme } from "../utils/toggleTheme"
+import styles from "../public/App.module.css"
+import { Link } from "react-router-dom"
+import { ThemeContext } from "../context/ThemeContext"
+import { useContext } from "react"
+
+const ContextAPI= () => {
+    const context = useContext<ThemeStateType>(ThemeContext)
+    const { theme } = context
+
+    return (
+    <div className={`${theme === "light" ? styles.light : styles.dark} ${styles.container}`}>
+        <header className={styles.header}>
+          <h1>This page is for Context API page</h1>
+          <button onClick={() => toggleTheme(context)}>
+            Switch to {theme === "light" ? 'Dark' : 'Light'} Mode
+          </button>
+        </header>
+          <Link to="/">
+            <button>
+                Back to Main
+            </button>
+          </Link>
+      </div>
+    )
+}
+
+export default ContextAPI
